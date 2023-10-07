757c1e5fa2f84cfe537d4f41a0259737f561c07c
Your Name <you@example.com>
2023-10-07 05:26:14 -0400

    google-news-300

--- app/api/endpoints/recommendation.py
+++ app/api/endpoints/recommendation.py
@@ -1,15 +1,20 @@
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from gensim.models import KeyedVectors
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import APIRouter, HTTPException, Depends
 import os
-from dotenv import load_dotenv
+import gensim.downloader as api
+
+# info = api.info()  # show info about available models/datasets
+model = api.load("word2vec-google-news-300")
+
+# Load the pre-trained Word2Vec model
+# word_vectors = KeyedVectors.load_word2vec_format('path/to/your/pretrained/model.bin', binary=True)
 
 MONGO_URI = os.environ.get("MONGO_URI")
 client = AsyncIOMotorClient(str(os.getenv("MONGODB_URL")))
 db = client.get_database("bring_the_menu_db")
-collection = db.get_collection("restaurant_info2")
+collection = db.get_collection("restaurant_info6")
 
 router = APIRouter()
 
@@ -17,35 +22,35 @@ async def fetch_data():
     cursor = collection.find({})
     data = await cursor.to_list(length=1000)
     return pd.DataFrame(data)
-
 async def get_recommendations(search_term: str):
     df = await fetch_data()
-    df['all_categories'] = df['categories'].apply(', '.join)
 
-    vectorizer = CountVectorizer()
-    count_matrix = vectorizer.fit_transform(df['all_categories'])
-    cosine_sim = cosine_similarity(count_matrix)
-
-    if search_term not in vectorizer.get_feature_names_out():
+    if search_term not in model.key_to_index:
         return []
 
-    idx = list(vectorizer.get_feature_names_out()).index(search_term)
-    sim_scores = list(enumerate(cosine_sim[idx]))
+    # Get the unique categories from your MongoDB collection
+    unique_categories = df['categories'].explode().unique()
 
-    for i in range(len(sim_scores)):
-        exact_match_weight = 1
-        rating_weight = float(df.iloc[i]['rating']) / 5  # Normalize the rating to a scale of 0 to 1
+    # Compute word similarities using Word2Vec model for categories that exist in your collection
+    word_similarities = []
+    for category in unique_categories:
+        if category in model.key_to_index:
+            similarities = model.most_similar(category, topn=10)
+            word_similarities.extend(similarities)
 
-        if search_term.lower() in df.iloc[i]['all_categories'].lower().split(', '):
-            exact_match_weight = 5  # Increase the weight for exact matches
-            
-        sim_scores[i] = (sim_scores[i][0], sim_scores[i][1] * rating_weight * exact_match_weight)
+    # Sort the word similarities by similarity score
+    word_similarities.sort(key=lambda x: x[1], reverse=True)
 
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[:3]  
-    food_indices = [i[0] for i in sim_scores]
+    # Filter the dataframe based on word similarities
+    similar_categories = [category for category, _ in word_similarities if category in unique_categories]
+    filtered_df = df[df['categories'].str.contains('|'.join(similar_categories), case=False, na=False)]
 
-    return [dict(item, _id=str(item['_id'])) for item in df.iloc[food_indices].to_dict('records')]
+    # Sort the filtered dataframe by rating and return top recommendations
+    filtered_df = filtered_df.sort_values(by='rating', ascending=False)
+    recommendations = filtered_df.head(3)
+
+    return [dict(item, _id=str(item['_id'])) for item in recommendations.to_dict('records')]
+
 
 
 @router.get("/recommendations/{search_term}")
